f879c3ceb3579bde01df9c3f676f128e0aeb67d1
Cooperr06 <colin.rinke@googlemail.com>
2022-07-12 19:09:19 +0200

    Changed from using task ID to using the task itself Added life generator when first joining server

--- src/main/java/de/cooperr/threelives/listener/PlayerJoinListener.java
+++ src/main/java/de/cooperr/threelives/listener/PlayerJoinListener.java
@@ -1,11 +1,17 @@
 package de.cooperr.threelives.listener;
 
 import de.cooperr.threelives.ThreeLives;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.title.Title;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
+import java.time.Duration;
 import java.util.Map;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class PlayerJoinListener implements Listener {
     
@@ -26,11 +32,40 @@ public class PlayerJoinListener implements Listener {
         if (!playersSection.contains(String.valueOf(player.getUniqueId()))) {
             playersSection.createSection(String.valueOf(player.getUniqueId()), Map.of(
                 "name", player.getName(),
-                "lives", 3,
+                "lives", -1,
                 "playtime", 0)
             );
+            var lives = showLivesGenerator(player);
+            
+            var playerSection = plugin.getConfig().getConfigurationSection("players." + player.getUniqueId());
+            assert playerSection != null;
+            
+            playerSection.set("lives", lives);
+            plugin.saveConfig();
         }
         
         plugin.getPlaytimeManager().startPlayerTimer(player);
+    }
+    
+    private int showLivesGenerator(Player player) {
+        var task = plugin.getServer().getScheduler().runTaskTimer(plugin, () -> {
+            var random = ThreadLocalRandom.current().nextInt(4, 7);
+            var component = Component.text(random,
+                random == 6 ? NamedTextColor.GREEN :
+                    random == 5 ? NamedTextColor.YELLOW :
+                        random == 4 ? NamedTextColor.RED : null);
+            
+            var title = Title.title(component, Component.text(""), Title.Times.times(Duration.ZERO, Duration.ofMillis(400), Duration.ZERO));
+            
+            player.showTitle(title);
+        }, 50, 8);
+        plugin.getServer().getScheduler().runTaskLater(plugin, task::cancel, 8 * 20);
+        
+        var lives = ThreadLocalRandom.current().nextInt(4, 7);
+        
+        var title = Title.title(Component.text(lives),
+            Component.text(""), Title.Times.times(Duration.ZERO, Duration.ofMillis(400), Duration.ZERO));
+        
+        return lives;
     }
 }
--- src/main/java/de/cooperr/threelives/util/Timer.java
+++ src/main/java/de/cooperr/threelives/util/Timer.java
@@ -2,25 +2,25 @@ package de.cooperr.threelives.util;
 
 import de.cooperr.threelives.ThreeLives;
 import lombok.Getter;
+import org.bukkit.scheduler.BukkitTask;
 
 public class Timer {
     
     private final ThreeLives plugin;
     @Getter
     private long time;
-    private int taskId;
+    private BukkitTask task;
     
     public Timer(ThreeLives plugin) {
         this.plugin = plugin;
     }
     
     public void start() {
-        taskId = plugin.getServer().getScheduler().scheduleSyncRepeatingTask(plugin, () -> time++, 20, 20);
+        task = plugin.getServer().getScheduler().runTaskTimer(plugin, () -> time++, 0, 20);
     }
     
     public void stop() {
-        plugin.getServer().getScheduler().cancelTask(taskId);
+        task.cancel();
         time = 0;
-        taskId = 0;
     }
 }
